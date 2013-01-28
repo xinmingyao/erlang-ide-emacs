f8ee0a78657d87d51239ed3f34095b3106b5cce4
xinming.yao <yaoxinming@gmail.com>
2013-01-28 17:36:07 +0800

    add support for deps dir

--- elisp/ecompile.sh
+++ elisp/ecompile.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env escript
 -export([main/1]).
 -define(TEST,true).
-main([File_Name]) ->
-    compile:file(File_Name, [warn_obsolete_guard, warn_unused_import, 
-                             warn_shadow_vars, warn_export_vars,
-  			     strong_validation, report,
-  			     {i, "../include"},{i,"./include"},{d,'TEST'}]).
+main([F,Base]) ->
+ 
+    Dep=filename:join([Base,"deps"]),
+    {ok,Dirs}=file:list_dir(Dep),
+    lists:map(fun(A)->
+		      Pa= filename:join([Dep,A,"ebin"]),
+		      code:add_path(Pa)
+			  ,Pa
+	      end ,Dirs),
+    Includes=lists:map(fun(A)->
+			       Pa= filename:join([Dep,A,"include"]),			       
+			       {i,Pa}
+		       end ,Dirs),
+    Opts=[warn_obsolete_guard, warn_unused_import, 
+	  warn_shadow_vars, warn_export_vars,
+	  strong_validation, report,
+	  {i, "../include"},{i,"./include"},{d,'TEST'}],
+    compile:file(F,lists:merge(Opts,Includes))
+		 .
 
